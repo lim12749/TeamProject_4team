2e862d5b0a1784ab4e2bef160e79728066d370dc
shilhun <50003828+lim12749@users.noreply.github.com>
2025-09-04 21:42:36 +0900

    커밋 내용은 깃 저장방법

--- Assets/asd.cs
+++ Assets/asd.cs
@@ -6,6 +6,7 @@ public class asd : MonoBehaviour
     void Start()
     {
         Debug.Log("test");
+        Debug.Log("test"); Debug.Log("test"); Debug.Log("test"); Debug.Log("test"); Debug.Log("test"); Debug.Log("test"); Debug.Log("test");
     }
 
     // Update is called once per frame
