1aa32232c5a53c6c271c72c98a3f454adcdd7a4a
shilhun <50003828+lim12749@users.noreply.github.com>
2025-09-11 19:10:15 +0900

    normailze line endings to LF

--- Assets/InputReader.cs
+++ Assets/InputReader.cs
@@ -21,8 +21,8 @@ public class InputReader : MonoBehaviour
         pi = GetComponent<PlayerInput>();
         var a = pi.actions;
         move = a.FindAction("Move", true);
-        look = a.FindAction("Look", true);
-        aim = a.FindAction("Aim", true);
+        //look = a.FindAction("Look", true);
+        //aim = a.FindAction("Aim", true);
         sprint = a.FindAction("Sprint", true);
         interact = a.FindAction("Interact", true);
         //reload   = a.FindAction("Reload",   true);
@@ -33,11 +33,11 @@ public class InputReader : MonoBehaviour
         move.performed += ctx => Move = ctx.ReadValue<Vector2>();
         move.canceled += _ => Move = Vector2.zero;
 
-        look.performed += ctx => Look = ctx.ReadValue<Vector2>();
-        look.canceled += _ => Look = Vector2.zero;
+        //look.performed += ctx => Look = ctx.ReadValue<Vector2>();
+        //look.canceled += _ => Look = Vector2.zero;
 
-        aim.started += _ => AimHeld = true;
-        aim.canceled += _ => AimHeld = false;
+        //aim.started += _ => AimHeld = true;
+        //aim.canceled += _ => AimHeld = false;
 
         sprint.started += _ => SprintHeld = true;
         sprint.canceled += _ => SprintHeld = false;
@@ -50,10 +50,10 @@ public class InputReader : MonoBehaviour
     {
         move.performed -= ctx => Move = ctx.ReadValue<Vector2>();
         move.canceled -= _ => Move = Vector2.zero;
-        look.performed -= ctx => Look = ctx.ReadValue<Vector2>();
-        look.canceled -= _ => Look = Vector2.zero;
-        aim.started -= _ => AimHeld = true;
-        aim.canceled -= _ => AimHeld = false;
+        //look.performed -= ctx => Look = ctx.ReadValue<Vector2>();
+        //look.canceled -= _ => Look = Vector2.zero;
+        //aim.started -= _ => AimHeld = true;
+        //aim.canceled -= _ => AimHeld = false;
         sprint.started -= _ => SprintHeld = true;
         sprint.canceled -= _ => SprintHeld = false;
         interact.performed -= _ => InteractPressed?.Invoke();
