668c6d5b89d8da74f69585f995e230dc851d24e4
Jaewoo Jung <choisdevil@naver.com>
2025-09-11 20:43:39 +0900

    코인
    코인 회전과 E키를 누르면 코인이 사라지며 텍스트가 1이 올라가는 기능을 만들었습니다.

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Coin : MonoBehaviour
 {
-    public float rotationSpeed = 200f;
-    // Update is called once per frame
+    public float rotationSpeed = 100f;
+    public KeyCode deleteKey = KeyCode.E;
+
     void Update()
     {
         transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(deleteKey))
+        {
+            Destroy(gameObject);
+            Debug.Log("코인이 삭제되었습니다.");
+            ScoreManager.Instance.AddScore(1);
+        }
     }
 }
